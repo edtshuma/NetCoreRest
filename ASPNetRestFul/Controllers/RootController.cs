d50039495fae5cbb9c2ce31970457886df796e35
Edmore Tshuma <tshumae@fbc.corp>
2020-03-30 12:37:48 +0200

    Initial commit

---
+++ ASPNetRestFul/Controllers/RootController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetRestFul.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : Controller
+    {
+        
+    }
+}
