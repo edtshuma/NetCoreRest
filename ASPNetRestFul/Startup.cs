498a4f87ae2aa53fa45f4863f546b539de403885
Edmore Tshuma <tshumae@fbc.corp>
2020-04-02 13:21:31 +0200

    Added DbContext

--- ASPNetRestFul/Startup.cs
+++ ASPNetRestFul/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +30,12 @@ namespace ASPNetRestFul
         {
             services.Configure<HotelInfo>(
                 Configuration.GetSection("Info"));
+
+
+            services.AddDbContext<HotelApiDbContext>( 
+                options => options.UseInMemoryDatabase("manchesterdb")
+            );
+
 
             services
                 .AddMvc(options =>
